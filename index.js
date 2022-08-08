c4c4a0460b425716774089e35d0bd50d716fc7ce
sepezho <sepezho@gmail.com>
2022-08-08 16:58:35 +0200

    a

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 var cors_proxy = require('cors-anywhere');
-var fs = require('fs');
 
-var host = "tonana-proxy.herokuapp.com";
-var port = process.env.PORT || 5555;
-
+var host = process.env.PORT ? '0.0.0.0' : '127.0.0.1';
+var port = process.env.PORT || 8080;
 cors_proxy.createServer({
-    httpsOptions: {},
-    originWhitelist: [],
+    originWhitelist: [], // Allow all origins
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, function() {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
-
