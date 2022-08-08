6b5012d8b505f7a6a9c7a40c08bad16b74e1ba53
sepezho <sepezho@gmail.com>
2022-08-08 16:25:35 +0200

    a

---
+++ index.js
@@ -0,0 +1,35 @@
+var cors_proxy = require('cors-anywhere');
+var fs = require('fs');
+
+var host = process.env.HOST || 'sepezho.com';
+var port = process.env.PORT || 5555;
+
+const privateKey = fs.readFileSync(
+    "/home/admin/conf/web/ssl.sepezho.com.key",
+    "utf8"
+  );
+const certificate = fs.readFileSync(
+    "/home/admin/conf/web/ssl.sepezho.com.crt",
+    "utf8"
+  );
+const ca = fs.readFileSync(
+  "/home/admin/conf/web/ssl.sepezho.com.ca",
+ "utf8"
+);
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
+
+cors_proxy.createServer({
+    httpsOptions: credentials,
+    originWhitelist: [],
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(port, host, function() {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
+
