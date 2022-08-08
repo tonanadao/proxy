59c2a2960f2ba890c8edef93528faa7d08a6f1c1
sepezho <sepezho@gmail.com>
2022-08-08 16:41:07 +0200

    a

--- index.js
+++ index.js
@@ -4,28 +4,8 @@ var fs = require('fs');
 var host = process.env.HOST || 'sepezho.com';
 var port = process.env.PORT || 5555;
 
-const privateKey = fs.readFileSync(
-    "/home/admin/conf/web/ssl.sepezho.com.key",
-    "utf8"
-  );
-const certificate = fs.readFileSync(
-    "/home/admin/conf/web/ssl.sepezho.com.crt",
-    "utf8"
-  );
-const ca = fs.readFileSync(
-  "/home/admin/conf/web/ssl.sepezho.com.ca",
- "utf8"
-);
-
-const credentials = {
-	key: privateKey,
-	cert: certificate,
-	ca: ca
-};
-
-
 cors_proxy.createServer({
-    httpsOptions: credentials,
+    httpsOptions: {},
     originWhitelist: [],
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
