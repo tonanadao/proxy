f4f50b776524527d07430e8b4b0ce2d20403f4fc
SEPEZHO <47150504+SEPEZHO@users.noreply.github.com>
2022-10-13 17:03:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var port = process.env.PORT || 8080;
 cors_proxy.createServer({
     originWhitelist: [], // Allow all origins
     requireHeader: ['origin', 'x-requested-with'],
-    //removeHeaders: ['cookie', 'cookie2']
+    removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, function() {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
